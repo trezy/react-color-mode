6e3d2c9a13c8638d3e7ff66e372919b144291ba1
Trezy <tre@trezy.com>
2020-12-22 14:37:27 -0600

    build: switch to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// Module imports
+const path = require('path')
+
+
+
+
+
+module.exports = {
+	devtool: 'source-map',
+	entry: './lib/index.js',
+	externals: {
+		process: '_',
+		react: 'React',
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: { loader: 'babel-loader' },
+			},
+		],
+	},
+	// optimization: {
+	// 	runtimeChunk: true,
+	// },
+	output: {
+		filename: 'index.js',
+		globalObject: 'this',
+		library: 'next-color-mode',
+		libraryTarget: 'umd',
+		path: path.resolve(__dirname, 'dist'),
+	},
+}
