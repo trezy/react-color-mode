094d36085585f91f8af4fcc36df5a12972272684
Trezy <tre@trezy.com>
2020-12-30 21:51:56 -0600

    test(colormodecontext): update tests for new API

--- __tests__/ColorModeContext.js
+++ __tests__/ColorModeContext.js
@@ -1,5 +1,6 @@
 // Module imports
 import {
+	act,
 	cleanup,
 	fireEvent,
 	render,
@@ -27,84 +28,187 @@ describe('ColorModeContextProvider', () => {
 	const testID = 'color-mode-context'
 	const buttonTestID = `${testID}-button`
 
+	beforeAll(() => {
+		Object.defineProperty(window, 'matchMediaListeners', {
+			writable: true,
+			value: {},
+		})
+
+		Object.defineProperty(window, 'matchMedia', {
+			writable: true,
+			value: jest.fn().mockImplementation(query => ({
+				matches: false,
+				media: query,
+				addEventListener: jest.fn().mockImplementation((eventType, f) => {
+					if (!window.matchMediaListeners[eventType]) {
+						window.matchMediaListeners[eventType] = []
+					}
+					window.matchMediaListeners[eventType].push(f)
+				}),
+				removeEventListener: jest.fn(),
+				dispatchEvent: jest.fn().mockImplementation((eventType, eventData) => {
+					if (window.matchMediaListeners[eventType]) {
+						window.matchMediaListeners[eventType].forEach(f => f(eventData))
+					}
+				}),
+			})),
+		})
+	})
+
 	describe('with Consumer', () => {
-		function testUpdateColorModeWithConfig(testName, config) {
+		function testUpdateColorModePreferenceWithConfig(testName, config) {
 			test(testName, () => {
 				// Force local color mode
-				setLocalColorModePreference((config.defaultConfig || {}), config.initialColorMode)
+				setLocalColorModePreference((config.defaultConfig || {}), config.initialColorModePreference)
 
 				const { getByTestId } = render((
 					<ColorModeContextProvider {...(config.defaultConfig || {})}>
 						<ColorModeContext.Consumer>
-							{({ colorMode, updateColorMode }) => (
-								<>
-									<div data-testid={testID}>{colorMode}</div>
-									<button
-										data-testid={buttonTestID}
-										onClick={() => updateColorMode(config.updatedColorMode)} />
-								</>
-							)}
+							{state => {
+								const {
+									colorMode,
+									colorModePreference,
+									systemColorMode,
+									updateColorModePreference,
+								} = state
+
+								return (
+									<>
+										<div data-testid={`${testID}-colorMode`}>{colorMode}</div>
+										<div data-testid={`${testID}-systemColorMode`}>{systemColorMode}</div>
+										<div data-testid={`${testID}-colorModePreference`}>{colorModePreference}</div>
+										<button
+											data-testid={buttonTestID}
+											onClick={() => updateColorModePreference(config.updatedColorModePreference)} />
+									</>
+								)
+							}}
 						</ColorModeContext.Consumer>
 					</ColorModeContextProvider>
 				))
 
 				// Verify the initial color mode
-				expect(getByTestId(testID).textContent).toEqual(config.initialColorMode)
+				expect(getByTestId(`${testID}-colorMode`).textContent).toEqual(config.initialColorMode)
+				expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual(config.systemColorMode)
+				expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual(config.initialColorModePreference)
 
 				// Trigger switch to dark mode
 				fireEvent.click(getByTestId(buttonTestID))
 
 				// Verify that the color mode has been updated
-				expect(getByTestId(testID).textContent).toEqual(config.updatedColorMode)
+				expect(getByTestId(`${testID}-colorMode`).textContent).toEqual(config.updatedColorMode)
+				expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual(config.systemColorMode)
+				expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual(config.updatedColorModePreference)
 			})
 		}
 
-		testUpdateColorModeWithConfig('with defaults', {
-			initialColorMode: 'system',
+		testUpdateColorModePreferenceWithConfig('with defaults', {
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'dark',
+			updatedColorModePreference: 'dark',
 		})
 
-		testUpdateColorModeWithConfig('with custom storage key', {
+		testUpdateColorModePreferenceWithConfig('with custom storage key', {
 			defaultConfig: {
 				storageKey: 'custom-storage-key',
 			},
-			initialColorMode: 'system',
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'dark',
+			updatedColorModePreference: 'dark',
 		})
 
-		testUpdateColorModeWithConfig('with custom default mode', {
+		testUpdateColorModePreferenceWithConfig('with custom default mode', {
 			defaultConfig: {
 				defaultMode: 'dark',
 			},
-			initialColorMode: 'system',
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'light',
+			updatedColorModePreference: 'light',
 		})
 
-		testUpdateColorModeWithConfig('with custom storage key and default mode', {
+		testUpdateColorModePreferenceWithConfig('with custom storage key and default mode', {
 			defaultConfig: {
 				defaultMode: 'dark',
 				storageKey: 'custom-storage-key',
 			},
-			initialColorMode: 'system',
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'light',
+			updatedColorModePreference: 'light',
+		})
+
+		test('settings update when system color mode changes', () => {
+			// Force local color mode preference
+			setLocalColorModePreference({}, 'system')
+
+			const { getByTestId } = render((
+				<ColorModeContextProvider>
+					<ColorModeContext.Consumer>
+						{state => {
+							const {
+								colorMode,
+								colorModePreference,
+								systemColorMode,
+							} = state
+
+							return (
+								<>
+									<div data-testid={`${testID}-colorMode`}>{colorMode}</div>
+									<div data-testid={`${testID}-systemColorMode`}>{systemColorMode}</div>
+									<div data-testid={`${testID}-colorModePreference`}>{colorModePreference}</div>
+								</>
+							)
+						}}
+					</ColorModeContext.Consumer>
+				</ColorModeContextProvider>
+			))
+
+			// Verify the initial color mode
+			expect(getByTestId(`${testID}-colorMode`).textContent).toEqual('light')
+			expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual('light')
+			expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual('system')
+
+			// Trigger switch to dark mode
+			act(() => {
+				window.matchMedia('(prefers-color-scheme: dark)').dispatchEvent('change', { matches: true })
+			})
+
+			// Verify that the color mode has been updated
+			expect(getByTestId(`${testID}-colorMode`).textContent).toEqual('dark')
+			expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual('dark')
+			expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual('system')
 		})
 	})
 
 	describe('with useColorMode', () => {
-		function testUpdateColorModeWithConfig(testName, config) {
+		function testUpdateColorModePreferenceWithConfig(testName, config) {
 			test(testName, () => {
 				// Force local color mode
 				setLocalColorModePreference((config.defaultConfig || {}), config.initialColorMode)
 
 				function Consumer() {
-					const { colorMode, updateColorMode } = useColorMode()
+					const {
+						colorMode,
+						colorModePreference,
+						systemColorMode,
+						updateColorModePreference,
+					} = useColorMode()
 
 					return (
 						<>
-							<div data-testid={testID}>{colorMode}</div>
+							<div data-testid={`${testID}-colorMode`}>{colorMode}</div>
+							<div data-testid={`${testID}-systemColorMode`}>{systemColorMode}</div>
+							<div data-testid={`${testID}-colorModePreference`}>{colorModePreference}</div>
 							<button
 								data-testid={buttonTestID}
-								onClick={() => updateColorMode(config.updatedColorMode)} />
+								onClick={() => updateColorModePreference(config.updatedColorModePreference)} />
 						</>
 					)
 				}
@@ -115,46 +219,107 @@ describe('ColorModeContextProvider', () => {
 					</ColorModeContextProvider>
 				))
 
-
 				// Verify the initial color mode
-				expect(getByTestId(testID).textContent).toEqual(config.initialColorMode)
+				expect(getByTestId(`${testID}-colorMode`).textContent).toEqual(config.initialColorMode)
+				expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual(config.systemColorMode)
+				expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual(config.initialColorModePreference)
 
 				// Trigger switch to dark mode
 				fireEvent.click(getByTestId(buttonTestID))
 
 				// Verify that the color mode has been updated
-				expect(getByTestId(testID).textContent).toEqual(config.updatedColorMode)
+				expect(getByTestId(`${testID}-colorMode`).textContent).toEqual(config.updatedColorMode)
+				expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual(config.systemColorMode)
+				expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual(config.updatedColorModePreference)
 			})
 		}
 
-		testUpdateColorModeWithConfig('with defaults', {
-			initialColorMode: 'system',
+		testUpdateColorModePreferenceWithConfig('with defaults', {
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'dark',
+			updatedColorModePreference: 'dark',
 		})
 
-		testUpdateColorModeWithConfig('with custom storage key', {
+		testUpdateColorModePreferenceWithConfig('with custom storage key', {
 			defaultConfig: {
 				storageKey: 'custom-storage-key',
 			},
-			initialColorMode: 'system',
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'dark',
+			updatedColorModePreference: 'dark',
 		})
 
-		testUpdateColorModeWithConfig('with custom default mode', {
+		testUpdateColorModePreferenceWithConfig('with custom default mode', {
 			defaultConfig: {
 				defaultMode: 'dark',
 			},
-			initialColorMode: 'system',
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'light',
+			updatedColorModePreference: 'light',
 		})
 
-		testUpdateColorModeWithConfig('with custom storage key and default mode', {
+		testUpdateColorModePreferenceWithConfig('with custom storage key and default mode', {
 			defaultConfig: {
 				defaultMode: 'dark',
 				storageKey: 'custom-storage-key',
 			},
-			initialColorMode: 'system',
+			initialColorMode: 'light',
+			initialColorModePreference: 'system',
+			systemColorMode: 'light',
 			updatedColorMode: 'light',
+			updatedColorModePreference: 'light',
+		})
+
+		test('settings update when system color mode changes', () => {
+			// Force local color mode preference
+			setLocalColorModePreference({}, 'system')
+
+			function Consumer() {
+				const {
+					colorMode,
+					colorModePreference,
+					systemColorMode,
+					updateColorModePreference,
+				} = useColorMode()
+
+				return (
+					<>
+						<div data-testid={`${testID}-colorMode`}>{colorMode}</div>
+						<div data-testid={`${testID}-systemColorMode`}>{systemColorMode}</div>
+						<div data-testid={`${testID}-colorModePreference`}>{colorModePreference}</div>
+						<button
+							data-testid={buttonTestID}
+							onClick={() => updateColorModePreference(config.updatedColorMode)} />
+					</>
+				)
+			}
+
+			const { getByTestId } = render((
+				<ColorModeContextProvider>
+					<Consumer />
+				</ColorModeContextProvider>
+			))
+
+			// Verify the initial color mode
+			expect(getByTestId(`${testID}-colorMode`).textContent).toEqual('light')
+			expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual('light')
+			expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual('system')
+
+			// Trigger switch to dark mode
+			act(() => {
+				window.matchMedia('(prefers-color-scheme: dark)').dispatchEvent('change', { matches: true })
+			})
+
+			// Verify that the color mode has been updated
+			expect(getByTestId(`${testID}-colorMode`).textContent).toEqual('dark')
+			expect(getByTestId(`${testID}-systemColorMode`).textContent).toEqual('dark')
+			expect(getByTestId(`${testID}-colorModePreference`).textContent).toEqual('system')
 		})
 	})
 })
